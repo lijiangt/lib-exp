de2ef28ca7e8b208105c415c7da44fd254c951e1
lijiangt@gmail.com <lijiangt@gmail.com@8f1899b2-6a43-79cd-e23c-0e9f0f8d3944>
2011-01-18 04:34:45 +0000

    增加了一些代码

--- test/testPathInfo/trunk/src/TestServlet.java
+++ test/testPathInfo/trunk/src/TestServlet.java
@@ -1,6 +1,7 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -34,7 +35,7 @@ import javax.servlet.http.HttpServletResponse;
 	/* (non-Javadoc)
 	 * @see javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
-	public void service(HttpServletRequest req, HttpServletResponse res)
+	public void service(HttpServletRequest request, HttpServletResponse res)
 			throws ServletException, IOException {
 		PrintWriter out = res.getWriter();
 		out.println("<html>");
@@ -42,22 +43,30 @@ import javax.servlet.http.HttpServletResponse;
 		out.println("<title>Test</title>");
 		out.println("</head>");
 		out.println("<body>");
-		out.println("req.getRequestURI():"+req.getRequestURI());
+		out.println("request.getRequestURI():"+request.getRequestURI());
 		out.println("<br />");
-		out.println("req.getRequestURL().toString():"+req.getRequestURL().toString());
+		out.println("request.getRequestURL().toString():"+request.getRequestURL().toString());
 		out.println("<br />");
-		out.println("req.getPathInfo():"+req.getPathInfo());		
+		out.println("request.getPathInfo():"+request.getPathInfo());		
 		out.println("<br />");
-		out.println("req.getPathTranslated():"+req.getPathTranslated());
+		out.println("request.getPathTranslated():"+request.getPathTranslated());
 		out.println("<br />");
-		out.println("req.getServletPath():"+req.getServletPath());
+		out.println("request.getServletPath():"+request.getServletPath());
 		out.println("<br />");
-		out.println("req.getQueryString():"+req.getQueryString());
+		out.println("request.getQueryString():"+request.getQueryString());
 		out.println("<br />");
-		for(Object k:req.getParameterMap().keySet()){
-			String s = (String)k;
-		out.println("parameter "+s+":"+req.getParameter(s));
-		out.println("<br />");
+        out.println("request.getRemoteAddr():"+request.getRemoteAddr());
+        out.println("<br />");
+		for(Object k:request.getParameterMap().keySet()){
+			String str = (String)k;
+			out.println("parameter "+str+":"+request.getParameter(str));
+			out.println("<br />");
+		}
+		java.util.Enumeration emu = request.getHeaderNames();
+		while(emu.hasMoreElements()){
+			String str2 = (String) emu.nextElement();
+			out.println("header "+str2+":"+request.getHeader(str2));
+			out.println("<br />");
 		}
 		out.println("</body>");	 
 		out.println("</html>");
